d4196da57fd34aa56ed194113ffc5934f7291400
dchandran371 <divyachandran716@gmail.com>
2026-08-01 12:12:07 +0530

    Fix production API URL

--- healthcare-frontend/src/api/axios.js
+++ healthcare-frontend/src/api/axios.js
@@ -3,43 +3,40 @@ import axios from "axios";
 
 const API = axios.create({
 
-baseURL:
-import.meta.env.VITE_API_URL ||
-"http://localhost:5000/api"
+  baseURL:
+    import.meta.env.VITE_API_URL ||
+    "https://healthcare-project-b0ir.onrender.com/api"
 
 });
 
 
-
 API.interceptors.request.use(
+
 (config)=>{
 
-
-const token =
-localStorage.getItem("token");
-
-
-if(token){
-
-config.headers.Authorization =
-`Bearer ${token}`;
-
-}
+  const token =
+  localStorage.getItem("token");
 
 
-return config;
+  if(token){
 
+    config.headers.Authorization =
+    `Bearer ${token}`;
+
+  }
+
+
+  return config;
 
 },
 
 (error)=>{
 
-return Promise.reject(error);
+  return Promise.reject(error);
 
 }
 
 );
-
 
 
 export default API;
